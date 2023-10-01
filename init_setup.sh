2e764a79a338084607c8142e48b9aeacb4de2b0e
yashanksh <yashankpandit420@gmail.com>
2023-10-01 17:02:59 +0530

    virtual env and requirement intallation

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
-echo $(date): "START" 
-echo $(date): "creating env with python 3.11 version" 
+echo [$(date)]: "START" 
+echo [$(date)]: "creating env with python 3.11 version" 
 conda create --prefix ./env python=3.11 -y
-echo $(date): "activating the environment" 
+echo [$(date)]: "activating the environment" 
 conda activate ./env
-echo $(date): "installing the dev requirements" 
+echo [$(date)]: "installing the dev requirements" 
 pip install -r requirements_dev.txt
-echo $(date): "END"
+echo [$(date)]: "END"
